e9d6eaeb060e756924c859dd2a395b005d87e58e
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2021-07-05 21:45:44 -0300

    finalizando score board e css

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let canvas = document.querySelector(".snake");
 let context = canvas.getContext("2d");
 let box = 32;
 
+let score = 0;
+
 let snake = [];
 snake[0] = {
   x: 8 * box,
@@ -40,11 +42,23 @@ function update(event) {
   if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
+function addPoints() {
+  score += 10;
+  document.querySelector("#score").innerHTML = `${score}`;
+}
+
 function iniciarJogo() {
   if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
   if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(jogo);
+      alert(`GameOver \n Score: ${score}`);
+    }
+  }
 
   criarBG();
   createSnake();
@@ -63,6 +77,8 @@ function iniciarJogo() {
   } else {
     food.x = Math.floor(Math.random() * 15 + 1) * box;
     food.y = Math.floor(Math.random() * 15 + 1) * box;
+
+    addPoints();
   }
 
   let newHead = {
